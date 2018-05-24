a8001dc967d14b2ed9b5ff8ca67841da678c6195
yuta <yuta10140514@gmail.com>
2018-05-24 23:27:19 +0900

    Created part of maze

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour
+{
+	public GameObject gameoverObject;
+
+	void OnTriggerEnter (Collider hit)
+	{
+		if (hit.CompareTag("Player")) {
+			// オブジェクトをアクティブにする
+			gameoverObject.SetActive (true);
+		}
+	}
+}
