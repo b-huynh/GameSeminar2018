c21bfe0c41e7bae7061102b83b6c8947c219e5a9
b-huynh <yukibhuynh@gmail.com>
2018-07-31 15:04:15 +0900

    Merge vive components

---
+++ GameSeminar2018_room-cec15b1f5288a22189d40f441fa60b9cba66fdd7/sample/Assets/Scripts/DisableOutOfView.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using HTC.UnityPlugin.StereoRendering;
+using Valve.VR.InteractionSystem;
+
+public class DisableOutOfView : MonoBehaviour {
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsInView()) {
+			EnablePortalRendering();
+		} else {
+			DisablePortalRendering();
+		}
+	}
+
+	public void DisablePortalRendering() {
+		StereoRenderer stereoRend = GetComponent<StereoRenderer>();
+		if (stereoRend == null)
+			return;
+
+		stereoRend.enabled = false;
+
+		foreach (Transform child in transform) {
+			child.gameObject.SetActive(false);
+		}
+	}
+
+	public void EnablePortalRendering() {
+		StereoRenderer stereoRend = GetComponent<StereoRenderer>();
+		if (stereoRend == null)
+			return;
+
+		stereoRend.enabled = true;
+
+		foreach (Transform child in transform) {
+			child.gameObject.SetActive(true);
+		}
+	}
+
+	public bool IsInView() {
+		Vector3 center = rend.bounds.center;
+		Vector3 cameraPoint = Camera.main.WorldToViewportPoint(center);
+		return cameraPoint.x >= 0 && cameraPoint.x <= 1 && cameraPoint.y >= 0 && cameraPoint.y <= 1;
+	}
+}
