149492f739783a906cb0f0ed658e261d7a7e8a5f
yuta005 <yuta10140514@gmail.com>
2018-07-31 10:53:52 +0900

    unity project commit

---
+++ GameSeminar2018_room-cec15b1f5288a22189d40f441fa60b9cba66fdd7/sample/Assets/SceneLoadScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+public class SceneLoadScript : MonoBehaviour {
+
+    private bool goalEntered;
+
+	//　スタートボタンを押したら実行する
+	public void GameStart() {
+		SceneManager.LoadScene ("Portal");
+	}
+
+    void LateUpdate() {
+        if (SceneManager.GetActiveScene().name == "Portal")
+        {
+            if (goalEntered)
+            {
+                SceneManager.LoadScene("Portal1");
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            goalEntered = true;
+        }
+    }
+}
